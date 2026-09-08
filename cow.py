c1e07daabd14a49a8b019e8a5db91112b29dfa88
avonhavensyr <anvonhav@syr.edu>
2026-09-08 12:45:58 -0400

    added cow.py to repository

---
+++ cow.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+m = 1                           # mass of cow [kg]
+ag = np.array([[0, -9.8]])      # acceleration due to gravity [m/s^2]
+
+t0 = np.array([0])              # start at time 0 [s]
+dt = 0.1                        # fixed time interval [s]
+hg = 0                          # ground height at 0 [m]
+
+r0x = 0                         # initial x-position                         
+r0y = 0                         # initial y-position
+v0 = 15                         # initial speed of 15 mph
+theta = np.pi/3                 # angle between v & the horizontal axis
+v0x = v0 * np.cos(theta)        # initial x-velocity      
+v0y = v0 * np.sin(theta)        # initial y-velocity
+
+r0 = np.array([[r0x, r0y]])     # initial position vector
+v0 = np.array([[v0x, v0y]])     # initial velocity vector
+
+
+# FUNCTIONS DESCRIBED IN THE ASSIGNMENT INSTRUCTIONS
+
+# 3a: Force as a function of position & velocity– assume only gravity & wind resistance
+def force(r, v, c):
+    """
+    Calculate the total force vector
+    r (2D array): Position Vector
+    v (2D array): Velocity Vector
+    """
+    # NOT CURRENTLY WORKING AS NEEDED
+    wr = c * (v**2)         # Wind resistance        
+    a = ag                  # Acceleration due to gravity
+    F = m * a              # NOT FINAL VERSION. ONLY INCLUDES GRAVITY.
+    return F
+
+# 3b: New position and velocity from the current position and velocity
+def newState(r, v, F, t, dt, c=1):
+    """
+    Function that takes the cow's current position
+    ,velocity, and force and returns a new position 
+    at a small time step later'
+    r (2D array): current position list
+    v (2D array): current velocity list
+    F (2D array): current force list
+    t (1D array): current time axis
+    dt: time interval
+    """
+
+    t0 = t[-1]
+    t0 += dt
+    t = np.append(t, t0)
+
+    F_tot = np.append(F, force(r[-1], v[-1], c), axis = 0)
+    F_new = F_tot[-1]
+
+    a = F_new / m
+    v_new = np.add(v, np.multiply(a, dt))
+    r_new = np.add(r, np.multiply(v_new, dt))
+    
+    r_tot = np.append(r, [r_new[-1]], axis=0)
+    v_tot = np.append(v, [v_new[-1]], axis=0)
+    #print(r[-1])
+
+    if r_tot[-1,1] < 0:
+        return r_tot, v_tot, F_tot, t, dt
+    else:
+        return newState(r_tot, v_tot, F_tot, t, dt)
+
+# Function to give total potential and total kinetic energy at a given instance
+def energy():
+    """
+    Function that calculates the potential, kinetic,
+    and total energy
+    """
+    pass
+
+
+# Running Code
+F0 = force(r0, v0, c=1)                                 # Force vector
+r, v, F, t, dt = newState(r0, v0, F0, t0, dt)           # New State
+
+x = r[:,0]                                              # x Positions
+y = r[:,1]                                              # y Positions
+
+plt.figure()
+fig, (ax1, ax2) = plt.subplots(2, 1)
+
+ax1.plot(x,y, label = 'y(x)')                           
+ax2.plot(t,x, label = 'x(t)')
+ax2.plot(t,y, label = 'y(t)')
+
+ax1.legend()
+ax2.legend()
+plt.tight_layout()
+plt.show()
+
